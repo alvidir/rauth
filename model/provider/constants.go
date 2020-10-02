830183a58e1b27e507b61e0d542fc836b5c6963f
Hèctor Morales <thehector1593@gmail.com>
2020-10-02 21:53:18 +0200

    updates

---
+++ model/provider/constants.go
@@ -0,0 +1,6 @@
+package provider
+
+const (
+	errProviderAlreadyExists = "Provider name already exists"
+	errNilProvider           = "Provider must not be nil"
+)
---
+++ model/provider/interfaces.go
@@ -0,0 +1,5 @@
+package provider
+
+// A Provider represents a cookie-kind session's provider
+type Provider interface {
+}
