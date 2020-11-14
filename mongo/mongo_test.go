8ca594f595eeab7b6757a888daf0e337c140afac
Hèctor Morales <thehector1593@gmail.com>
2020-11-14 11:18:03 +0100

    updates

--- mongo/mongo_test.go
+++ mongo/mongo_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestMongoClientConnection(t *testing.T) {
 	if err := godotenv.Load("../.env"); err != nil {
-		t.Fatalf("Got error %s; while loading dotenv", err.Error())
+		t.Fatalf("Got error %s, while loading dotenv", err.Error())
 	}
 
 	ctx := context.Background()
@@ -18,14 +18,14 @@ func TestMongoClientConnection(t *testing.T) {
 
 	client, err := NewMongoClient(ctx)
 	if err != nil {
-		t.Fatalf("Got %s; while getting the Mongo client", err.Error())
+		t.Fatalf("Got %s, while getting the Mongo client", err.Error())
 	}
 
 	if err = client.Ping(ctx, nil); err != nil {
-		t.Fatalf("Got %s; while sending ping to Mongo database", err.Error())
+		t.Fatalf("Got %s, while sending ping to Mongo database", err.Error())
 	}
 
 	if err = client.Disconnect(ctx); err != nil {
-		t.Fatalf("Got %s; while disconnecting from the MongoDB", err.Error())
+		t.Fatalf("Got %s, while disconnecting from the MongoDB", err.Error())
 	}
 }
