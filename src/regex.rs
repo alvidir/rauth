ff7b6d5e100f6a8a8b78f2df573334fb327aadc4
Hèctor Morales <thehector1593@gmail.com>
2021-02-27 16:24:52 +0100

    clean up: no more warnings

--- src/regex.rs
+++ src/regex.rs
@@ -5,14 +5,14 @@ use crate::default;
 const REGEX_NAME: &str = r"^[-_A-Za-z0-9\.]+$";
 const REGEX_EMAIL: &str = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$";
 const REGEX_HASH: &str = r"\b[A-Fa-f0-9]{8, 64}\b";
-//const REGEX_B64: &str = r"^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$";
+const _REGEX_B64: &str = r"^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$";
 const REGEX_URL: &str = r#"https?://(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"#;
 const REGEX_COOKIE: &str = r"^[A-Za-z0-9)(*&^%$#@!~?\]\[+-]+$";
 
 const ERR_NAME_FORMAT: &str = "The Name only allows alphanumeric characters";
 const ERR_EMAIL_FORMAT: &str = "The provided email does not match with any real address";
 const ERR_PWD_FORMAT: &str = "The provided password does not match the hash format";
-//const ERR_DATA_FORMAT: &str = "The provided data does not match the base 64 format";
+const _ERR_DATA_FORMAT: &str = "The provided data does not match the base 64 format";
 const ERR_URL_FORMAT: &str = "The provided string does not match with the url standard";
 const ERR_COOKIE_FORMAT: &str = "The provided cookie does not match with a real one";
 
@@ -43,14 +43,14 @@ pub fn match_pwd(pwd: &str) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-//pub fn match_base64(data: &str) -> Result<(), Box<dyn Error>> {
-//    let regex = Regex::new(REGEX_B64).unwrap();
-//    if !regex.is_match(data) {
-//        return Err(ERR_DATA_FORMAT.into());
-//    }
-//
-//    Ok(())
-//}
+pub fn _match_base64(data: &str) -> Result<(), Box<dyn Error>> {
+    let regex = Regex::new(_REGEX_B64).unwrap();
+    if !regex.is_match(data) {
+        return Err(_ERR_DATA_FORMAT.into());
+    }
+
+    Ok(())
+}
 
 pub fn match_url(data: &str) -> Result<(), Box<dyn Error>> {
     let regex = Regex::new(REGEX_URL).unwrap();
