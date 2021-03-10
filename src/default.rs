a5db0c6f6de518d70bbb83c14f56e97d5d0419c4
hector <thehector1593@gmail.com>
2021-03-10 23:24:36 +0100

    deployment changes: all project does now works via podman-compose, migration files has been restructured as required by the compose engine

--- src/default.rs
+++ src/default.rs
@@ -3,6 +3,9 @@ pub const SERVER_IP: &str = "127.0.0.1";
 pub const TOKEN_LEN: usize = 8;
 pub const TOKEN_TIMEOUT: u64 = 86400; // 3600s * 24h
 
+pub const CONNECTION_TIMEOUT: u64 = 100; // in seconds
+pub const CONNECTION_SLEEP: u64 = 1; // in seconds
+
 pub const RSA_NAME: &str = "default_rsa.pem";
 
 pub const ENV_SERVICE_PORT: &str = "SERVICE_PORT";
--- src/postgres.rs
+++ src/postgres.rs
@@ -1,3 +1,5 @@
+use std::time::{SystemTime, Duration};
+use std::thread;
 use std::env;
 use lazy_static;
 use diesel::{
@@ -20,10 +22,26 @@ struct Stream {
 lazy_static! {
     static ref STREAM: Stream = {
        Stream {
-           db_connection: PgPool::builder()
-               .max_size(POOL_SIZE)
-               .build(ConnectionManager::new(env::var(default::ENV_POSTGRES_DSN).expect(ERR_NOT_URL)))
-               .expect(ERR_CONNECT)
+           db_connection: {
+                let postgres_url = env::var(default::ENV_POSTGRES_DSN).expect(ERR_NOT_URL);
+                let start = SystemTime::now();
+                let timeout = Duration::from_secs(default::CONNECTION_TIMEOUT);
+                let sleep = Duration::from_secs(default::CONNECTION_SLEEP);
+
+                let mut pool = PgPool::builder().max_size(POOL_SIZE).build(ConnectionManager::new(&postgres_url));
+                while let Err(err) = pool {
+                    println!("{}: {}", ERR_CONNECT, err);
+                    let lapse = SystemTime::now().duration_since(start).unwrap();
+                    if  lapse < timeout  {
+                        thread::sleep(sleep);
+                        pool = PgPool::builder().max_size(POOL_SIZE).build(ConnectionManager::new(&postgres_url));
+                    } else {
+                        panic!(ERR_CONNECT);
+                    }
+                }
+
+                pool.unwrap()
+            },
         }
     };
 }
@@ -34,4 +52,17 @@ pub fn open_stream() -> &'static PgPool {
 
 pub fn must_connect() {
     open_stream().get().expect(ERR_CONNECT);
+    //let start = SystemTime::now();
+    //let timeout = Duration::from_secs(default::CONNECTION_TIMEOUT);
+    //let sleep = Duration::from_secs(default::CONNECTION_SLEEP);
+    //
+    //while let Err(err) = open_stream().get() {
+    //    println!("{}: {}", ERR_CONNECT, err);
+    //    let lapse = start.duration_since(SystemTime::now()).unwrap();
+    //    if  lapse > timeout  {
+    //        panic!(ERR_CONNECT);
+    //    } else {
+    //        thread::sleep(sleep);
+    //    }
+    //}
 }
