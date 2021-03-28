da9337ac5c272f3fb702f32d4bf2dd535ea1316b
Hèctor <thehector1593@gmail.com>
2021-03-28 03:16:48 +0200

    updates

--- migrations/up/040_user.sql
+++ migrations/up/040_user.sql
@@ -2,7 +2,7 @@ CREATE TABLE Users (
     id SERIAL PRIMARY KEY,
     client_id INTEGER NOT NULL UNIQUE,
     email VARCHAR(64) NOT NULL UNIQUE,
-    pwd VARCHAR(16) NOT NULL,
+    pwd VARCHAR(64) NOT NULL,
 
     FOREIGN KEY (client_id)
         REFERENCES Clients(id)
--- migrations/up/060_secret.sql
+++ migrations/up/060_secret.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Secrets (
     id SERIAL PRIMARY KEY,
     client_id INTEGER NOT NULL UNIQUE, 
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(256) NOT NULL,
     document TEXT NOT NULL UNIQUE,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     deadline TIMESTAMP,
