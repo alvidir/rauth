1a9b2d250029eb5326cb4013dfd4b6ea5cc70802
Hèctor <thehector1593@gmail.com>
2021-07-20 20:49:18 +0200

    build_db_setup_script simplified

--- scripts/build_db_setup_script.py
+++ scripts/build_db_setup_script.py
@@ -1,40 +1,31 @@
 #!/usr/bin/python3
 
-import sys
 import os
 import re
 
-def main(argv):
+TARGET_DIR = ".postgres"
+TARGET_FILE = "setup.sql"
+WORKING_PATH = "migrations"
+REGEX = "up.sql"
 
-    TARGET_DIR = ".postgres"
-    TARGET_FILE = "setup.sql"
-    WORKING_PATH = "migrations"
-    REGEX = "up.sql"
-    
-    if len(argv) > 0:
-        WORKING_PATH = str(argv[0])
+dir = os.path.join(WORKING_PATH, TARGET_DIR)
+if not os.path.exists(dir):
+    os.mkdir(dir)
 
-    dir = os.path.join(WORKING_PATH, TARGET_DIR)
-    if not os.path.exists(dir):
-        os.mkdir(dir)
+regex = re.compile(REGEX)
+target = open(os.path.join(dir, TARGET_FILE), "w")
 
-    regex = re.compile(REGEX)
-    target = open(os.path.join(dir, TARGET_FILE), "w")
-    
-    for root, dirs, files in os.walk(WORKING_PATH):
-        for file in files:
-            if regex.match(file):
-                path = os.path.join(root, file)
-                print("Reading content from {}".format(path))
+for root, dirs, files in os.walk(WORKING_PATH):
+    for file in files:
+        if regex.match(file):
+            path = os.path.join(root, file)
+            print("Reading content from {}".format(path))
 
-                fo = open(path, "r")
-                content = fo.read()
-                fo.close()
+            fo = open(path, "r")
+            content = fo.read()
+            fo.close()
 
-                target.write(content)
-                target.write("\n")
-    
-    target.close()
+            target.write(content)
+            target.write("\n")
 
-if __name__ == "__main__":
-   main(sys.argv[1:])
+target.close()
