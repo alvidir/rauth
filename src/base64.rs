3fe444c31a594cfcf33ea7e7b0255fd6205be94c
Hector <hector.morales.carnice@gmail.com>
2023-04-26 20:42:18 +0200

    fix: use closure for handler body

--- src/base64.rs
+++ src/base64.rs
@@ -1,5 +1,6 @@
 //! Base64 related utilities like custom engines for specific encodings/decodings.
 
+use crate::result::{Error, Result};
 use base64::{
     alphabet,
     engine::{self, general_purpose},
@@ -11,9 +12,22 @@ pub const B64_CUSTOM_ENGINE: engine::GeneralPurpose =
     engine::GeneralPurpose::new(&alphabet::URL_SAFE, general_purpose::NO_PAD);
 
 /// Decodes a b64 string
-pub fn decode_str(s: &str) -> Result<String, String> {
+pub fn decode_str(s: &str) -> Result<String> {
     B64_CUSTOM_ENGINE
         .decode(s)
-        .map_err(|err| err.to_string())
-        .and_then(|value| String::from_utf8(value).map_err(|err| err.to_string()))
+        .map_err(|err| {
+            warn!("{} decoding base64 string: {}", Error::Unknown, err);
+            Error::Unknown
+        })
+        .and_then(|value| {
+            String::from_utf8(value).map_err(|err| {
+                warn!(
+                    "{} getting string from base64 decoded data: {}",
+                    Error::Unknown,
+                    err
+                );
+
+                Error::Unknown
+            })
+        })
 }
--- src/http.rs
+++ src/http.rs
@@ -1,18 +1,45 @@
 use crate::base64;
-use crate::result::Error;
-use actix_web::HttpRequest;
+use crate::result::{Error, Result};
+use actix_web::{HttpRequest, HttpResponse};
 
-fn get_header(req: HttpRequest, header: &str) -> Result<String, String> {
+impl From<Error> for HttpResponse {
+    fn from(value: Error) -> Self {
+        match value {
+            Error::Unknown => HttpResponse::InternalServerError().body(value.to_string()),
+            Error::NotFound => HttpResponse::NotFound().finish(),
+            Error::NotAvailable => HttpResponse::ServiceUnavailable().finish(),
+            Error::Unauthorized => HttpResponse::Unauthorized().finish(),
+            Error::InvalidToken | Error::InvalidFormat | Error::InvalidHeader => {
+                HttpResponse::BadRequest().body(value.to_string())
+            }
+
+            Error::WrongCredentials => HttpResponse::Forbidden().finish(),
+            Error::RegexNotMatch => HttpResponse::NotAcceptable().finish(),
+        }
+    }
+}
+
+fn get_header(req: HttpRequest, header: &str) -> Result<String> {
     req.headers()
         .get(header)
-        .ok_or(Error::NotFound.to_string())
-        .and_then(|header| header.to_str().map_err(|err| err.to_string()))
+        .ok_or(Error::NotFound)
+        .and_then(|header| {
+            header.to_str().map_err(|err| {
+                warn!(
+                    "{} parsing header data to str: {}",
+                    Error::InvalidHeader,
+                    err
+                );
+
+                Error::InvalidHeader
+            })
+        })
         .map(Into::into)
 }
 
 /// Given an http request, returns the base64 decoded value of the provided header's key if any, otherwise
 /// an error is returned.
-pub fn get_encoded_header(req: HttpRequest, header: &str) -> Result<String, String> {
+pub fn get_encoded_header(req: HttpRequest, header: &str) -> Result<String> {
     let header = get_header(req, header)?;
     base64::decode_str(&header)
 }
